140267d47f7287acd3bbbf70e67c31e2ddf390b0
Jakob Felix Rieckers <119401962+ham-jaky@users.noreply.github.com>
2023-12-29 01:22:39 +0100

    Added Placeholders

--- bus-data/script.js
+++ bus-data/script.js
@@ -1,9 +1,14 @@
 var attr_osm = 'Map data &copy; <a href="http://openstreetmap.org/">OpenStreetMap</a> contributors';
 var attr_overpass = 'POI via <a href="http://www.overpass-api.de/">Overpass API</a>';
 
-var map = L.map('map').setView([52.5326, 13.8347], 11);
+var map = L.map('map')
+map.setView([52.5326, 13.8347], 11);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: [attr_osm, attr_overpass].join(', ')
 }).addTo(map);
+
+function getBusRouteMetas() {
+    
+}
