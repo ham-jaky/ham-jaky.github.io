91a273489e7d70c4912fd47abd784e36896e6e16
Jakob Felix Rieckers <119401962+ham-jaky@users.noreply.github.com>
2023-12-30 22:58:50 +0100

    Moved to from session to local storage

--- bus-data/script.js
+++ bus-data/script.js
@@ -1,23 +1,21 @@
 const attr_osm = 'Map data &copy; <a href="http://openstreetmap.org/">OpenStreetMap</a> contributors';
 const attr_overpass = 'POI via <a href="http://www.overpass-api.de/">Overpass API</a>';
-const all_data = `
+const query_osm_id = `
 [out:json][timeout:25];
-nwr[\"type\"=\"route\"][\"route\"=\"bus\"][\"operator\"~\"mobus\"];
+nwr[\"type\"=\"route\"][\"route\"=\"bus\"][\"operator\"~\"mobus\"](OSMID);
 out;`;
 const query_meta = `
 [out:json][timeout:25];
 nwr[\"type\"=\"route\"][\"route\"=\"bus\"][\"operator\"~\"mobus\"];
 out tags;`;
 const overpassUrl = 'https://overpass-api.de/api/interpreter';
-const sideMenuElement = "<a href=\"?osmid=OSMID\"><h4>Bus BUSREF (OPERATOR)</h4>BUSNAME</a>";
+const sideMenuElement = "<a href=\"#OSMID\"><h4>Bus BUSREF (OPERATOR)</h4>BUSNAME</a>";
 
 const urlParams = new URLSearchParams(window.location.search);
 const requestedID = urlParams.get('osmid');
 
 function setSideBarDivContent(content){
-    document.getElementById('side-menu-bus-routes').innerHTML = '';
     document.getElementById("side-menu-bus-routes").innerHTML = content;
-    
 }
 
 async function createSideMenuFromOverpass() {
@@ -31,29 +29,26 @@ async function createSideMenuFromOverpass() {
             div_content += tmp;
         }
         setSideBarDivContent(div_content)
-        sessionStorage.setItem("side-menu-div-content", div_content);
+        localStorage.setItem("side-menu-div-content", div_content);
     })
     .catch(error => {
         console.error('Fehler beim Abrufen der Overpass API-Daten:', error);
     });
 }
 
-function createSideMenu(loadFromSessionStorage=true) {
-    const storedDivContent = sessionStorage.getItem("side-menu-div-content");
-    if (storedDivContent && loadFromSessionStorage) {
-        console.debug("Loaded Data from session storage");
+function createSideMenu(loadFromStorage=true) {
+    const storedDivContent = localStorage.getItem("side-menu-div-content");
+    if (storedDivContent && loadFromStorage) {
         setSideBarDivContent(storedDivContent);
     } else {
-        console.debug("Else-block");
         createSideMenuFromOverpass();
     };
 }
 
 function createMap() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const myParam = urlParams.get('osmid');
-    if (myParam) {
-        console.log(myParam)
+    var osmid = window.location.hash.substring(1);
+    if (osmid) {
+        console.log(osmid)
     };
     var map = L.map('map');
     map.setView([52.5326, 13.8347], 11);
